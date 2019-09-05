9d7e8cca030263271f7dae8b0a531bbb98b52994
vieiramanda11 <amanda_vieira@outlook.com>
2019-09-05 12:54:28 -0300

    adding test

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'rest-client'
 
 resources = RestClient.get 'https://www.bing.com/search', {params: {'Ruby'}}
-
+begin
 puts resources.code
 puts -------------
 puts resources.headers
@@ -10,3 +10,6 @@ puts resources.cookies
 puts -------------
 puts resources.body
 
+
+puts RestClient::Request.execute(method: :get, url: 'https://www.bing.com/search',
+  timeout: 10)
