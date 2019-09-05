ac3b1b013d611a9c18f400dab50bec4a2c92e0d3
vieiramanda11 <amanda_vieira@outlook.com>
2019-09-05 16:23:33 -0300

    fixing lint errors

--- main.rb
+++ main.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 
-puts "Enter something to search: "
+puts 'Enter something to search: '
 search_val = gets.chomp
-resources = RestClient.get 'https://www.bing.com/search', {params: {q: search_val}}
+resources = RestClient.get 'https://www.bing.com/search', params: { q: search_val }
 
-puts "Status: "
+puts 'Status: '
 puts resources.code
 puts '-------------'
-puts "Headers: "
+puts 'Headers: '
 puts resources.headers
 puts '-------------'
-puts "Cookies: "
+puts 'Cookies: '
 puts resources.cookies
 puts '-------------'
-puts "Body: "
+puts 'Body: '
 puts resources.body
